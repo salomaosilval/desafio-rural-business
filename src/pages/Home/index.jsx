54f4bec7d6f102362da3990b00c0d173ac695e8c
Salomão Silva <salomaosilval@hotmail.com>
2023-03-26 00:27:38 -0400

    :sparkles: feat: add Home Page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+import { Form } from "../../components/Form";
+
+export const Home = () => {
+  return (
+    <>
+      <Header imageSource="https://www.rbvideo.com.br/img/logo.png" />
+      <Form />
+    </>
+  );
+};
